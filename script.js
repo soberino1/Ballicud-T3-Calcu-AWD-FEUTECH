893bc654f23568f3a8b98aeb7d6482e1b538d748
sxber <155806006+soberino1@users.noreply.github.com>
2024-03-10 20:11:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function doMath(oms){
+
+    var result = oms;
+    document.form1.answer.value += result;
+    }
+    
+    function equal (){
+    
+       form1.answer.value = eval(form1.answer.value);
+    }
+    
+    function clear1 (omsim){
+        var output = document.getElementById("display").value = "";
+    }
